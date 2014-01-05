857484e2e243b9ed937dab48f8ae387aaf25e324
Zack Michener <zack@zackmichener.net>
2014-01-05 01:54:47 -0800

    fix height/width change issue for default element rendering

--- src/element.js
+++ src/element.js
@@ -1,8 +1,6 @@
 var Element = (function() {
 
   var ElementConstr = function(I) {
-    var b;
-
     this.x = 0;
     this.y = 0;
     this.angle = 0;
@@ -10,12 +8,11 @@ var Element = (function() {
     this.width = 100;
     this.height = 100;
 
-    b = new Buffer(this.width, this.height);
-
     this.update = function() {
     };
 
     this.render = function() {
+      var b = new Buffer(this.width, this.height);
       b.clearRect(0, 0, this.width, this.height);
       b.fillStyle = 'black';
       b.fillRect(0, 0, this.width, this.height);
