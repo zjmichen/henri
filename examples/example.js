713c699474ae270a95904ae7129678b523711cd5
Zack Michener <zack@zackmichener.net>
2014-01-05 01:54:59 -0800

    implement clock example

--- examples/example.js
+++ examples/example.js
@@ -1,91 +1,81 @@
 window.onload = function() {
-  var s = new Stage(document.getElementById('example'));
-  s.toroidial = true;
+  var s = new Stage(document.getElementById('example')),
+      i, n;
 
-  var e = s.addElement(0, Ship, {});
-  var f = s.addElement(0, Ship, {x: 30});
-  var b = s.addElement(0, Bomb, {});
-  b.x = 50;
-  b.y = 50;
+  for (i = 0; i < 12; i++) {
+    n = s.addElement(0, Numeral, {});
+    n.value = i+1;
 
-  f.angle = Math.PI * 0.25;
-  f.x = 100;
-  f.y = 300;
-  f.scale = 0.5;
+    n.x = 0.5*(s.width - n.width) * Math.sin( (i+1) / 12 * 2*Math.PI) + 0.5*s.width;
+    n.y = -0.5*(s.height - n.height) * Math.cos( (i+1) / 12 * 2*Math.PI) + 0.5*s.height;
+  }
 
-  f.update = function() {
-    this.sprite.update();
-    this.scale += 0.005*Math.sin(0.05*this.stage.frame);
+  var hrHand = s.addElement(1, Hand, {});
+  hrHand.width = 350;
+  hrHand.height = 10;
+  hrHand.x = 0.5*s.width;
+  hrHand.y = 0.5*s.height;
+
+  hrHand.update = function() {
+    var d = new Date(),
+        hours = d.getHours() + (d.getMinutes() / 60);
+
+    this.angle = (hours / 12) * Math.PI * 2 - 0.5*Math.PI;
   };
 
-  f.sprite.setMode('thrusting');
+  var minHand = s.addElement(1, Hand, {});
+  minHand.width = 450;
+  minHand.height = 10;
+  minHand.x = 0.5*s.width;
+  minHand.y = 0.5*s.height;
 
-  s.at(100, function() {
-    e.die();
-  });
+  minHand.update = function() {
+    var d = new Date(),
+        mins = d.getMinutes() + (d.getSeconds() / 60);
 
+    this.angle = (mins / 60) * Math.PI * 2 - 0.5*Math.PI;
+  };
+
+  var secHand = s.addElement(1, Hand, {});
+  secHand.width = 450;
+  secHand.height = 2;
+  secHand.x = 0.5*s.width;
+  secHand.y = 0.5*s.height;
+
+  secHand.update = function() {
+    var d = new Date(),
+        secs = d.getSeconds() + (d.getMilliseconds() / 1000);
+
+    this.angle = (secs / 60) * Math.PI * 2 - 0.5*Math.PI;
+  };
 
   s.start();
-
-  window.f = f;
 }
 
-var Ship = function() {
-  var dying = false;
-
-  this.sprite = new Sprite(this.width, this.height);
-
-  this.sprite.addImage('normal', 'ship_normal.png');
-  this.sprite.addImage('thrusting', 'ship_fire1.png');
-  this.sprite.addImage('thrusting', 'ship_fire2.png');
-  this.sprite.addImage('thrusting', 'ship_fire3.png');
-  this.sprite.addImage('dead', 'explosion1.png');
-  this.sprite.addImage('dead', 'explosion2.png');
-
-  this.type = 'ship';
-  this.x = 250;
-  this.y = 200;
-  this.width = 150;
-  this.height = 64;
-
-  this.update = function() {
-    if (dying) {
-      this.scale *= 0.9;
-      if (this.scale <= 0.1) {
-        this.removeFromStage();
-      }
-    }
-    this.sprite.update();
-  };
+var Numeral = function() {
+  var b = new Buffer(50, 50);
+  this.value = 0;
+  this.y = 100;
+  this.width = 50;
+  this.height = 50;
 
   this.render = function() {
-    return this.sprite.render();
-  };
+    b.clearRect(0, 0, this.width, this.height);
+    b.fillText(this.value, 0.5*this.width, 0.5*this.height);
+    b.font = '16px sans-serif';
 
-  this.die = function() {
-    dying = true;
-    this.sprite.setMode('dead');
-    this.width = 50;
-    this.height = 50;
+    return b.canvas;
   };
 };
 
-var Bomb = function() {
-  this.sprite = new Sprite(this.width, this.height);
-  this.sprite.addImage('normal', 'bomb.png');
-
-  this.width = 15;
-  this.height = 15;
-  this.direction = 0.25*Math.PI;
-  this.speed = 0.5;
-
-  this.update = function() {
-    this.sprite.update();
-    this.x += this.speed*Math.cos(this.direction);
-    this.y += this.speed*Math.sin(this.direction);
-  }
+var Hand = function() {
+  this.angle = -0.5*Math.PI;
 
   this.render = function() {
-    return this.sprite.render();
-  }
-};
+    var b = new Buffer(this.width, this.height);
+
+    b.fillRect(0.5*this.width, 0, 0.5*this.width, this.height);
+
+    return b.canvas;
+  };
+}
