25f88b6325803b9f95d264987fa9f6b48e2b6544
Zack Michener <zackm@accretivetg.com>
2014-01-06 11:07:09 -0800

    add moveTo to clock example

--- examples/example.js
+++ examples/example.js
@@ -1,13 +1,18 @@
 window.onload = function() {
   var shipStage = new Stage(document.getElementById('ship')),
-      clockStage = new Stage(document.getElementById('clock'));
+      clockStage = new Stage(document.getElementById('clock')),
+      clock;
 
   shipStage.toroidial = true;
   shipStage.debug = true;
   shipStage.addElement(0, Ship, {x: 100, y: 100});
 
   clockStage.debug = true;
-  clockStage.addElement(0, Clock, {width: 200, height: 200, x: 250, y:250});
+  clock = clockStage.addElement(0, Clock, {width: 200, height: 200, x: 250, y:250});
+
+  clockStage.at(100, function() {
+    clock.moveTo(100, 100, 50);
+  });
 
   shipStage.start();
   clockStage.start();
